139edf18ec6547f08995e882a80683a334fe48a7
Adam Ford <ford.adm@gmail.com>
2024-09-22 22:17:31 -0500

    Added functions and got the basic css and html written

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNum = 0;
+let secondNum = 0;
+let solution = 0;
+let operator; 
+
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function sub (num1, num2) {
+    return num1 - num2;
+}
+
+function multi (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1/num2;
+}
+
+firstNum = 5;
+secondNum = 3;
+
+console.log(add(firstNum, secondNum));
